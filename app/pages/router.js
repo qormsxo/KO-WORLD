6fda08ad5ca8cbe5b834693d63e096d18bc21857
qormsxo <45680973+qormsxo@users.noreply.github.com>
2022-06-09 17:39:08 +0900

    FEAT : LOGIN

--- app/pages/router.js
+++ app/pages/router.js
@@ -1,8 +1,10 @@
 module.exports = function (app) {
-	// app setting
-	require('./express')(app); //현재폴더(project_test/app/connect) 기준 미들웨어 사용 설정
+  // app setting
+  require("./express")(app); //현재폴더(project_test/app/connect) 기준 미들웨어 사용 설정
 
-	require('../routers/main')(app);
+  require("../routers/main")(app);
 
-	require('../../passport/passport.js')(); // 로그인
+  require("../routers/login")(app);
+
+  require("../../passport/passport.js")(); // 로그인
 };
--- passport/passport.js
+++ passport/passport.js
@@ -1,10 +1,11 @@
-const passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
-const mariadb = require('mariadb'),
-    config = require('config'),
-    crud = require('../app/model/crud.js'),
-    requestIp = require('request-ip');
-const bcrypt = require('bcryptjs');
+const passport = require("passport"),
+    LocalStrategy = require("passport-local").Strategy;
+const mariadb = require("mariadb"),
+    config = require("config"),
+    crud = require("../app/model/crud.js"),
+    requestIp = require("request-ip");
+const bcrypt = require("bcrypt");
+const rounds = 10;
 
 module.exports = () => {
     // var ip_all;
@@ -14,35 +15,45 @@ module.exports = () => {
         //console.log("passport session", user) //req.session.passport.user에 저장
         done(null, user[0]);
     });
-    passport.deserializeUser((user, done) => { // 매개변수 id는 req.session.passport.user에 저장된 값
-        done(null, user) // 여기의 user가 req.user가 됨
+    // 매개변수 id는 req.session.passport.user에 저장된 값
+    passport.deserializeUser((user, done) => {
+        done(null, user); // 여기의 user가 req.user가 됨
     });
 
-    passport.use(new LocalStrategy({
-        usernameField: 'username',
-        passwordField: 'password',
-        session: true, // 세션에 저장 여부
-        passReqToCallback: true,
-    }, (req, username, password, done) => {
-        
-        var sql = "select * from admin_tb where id = ? ";
-        
-        var filter_data = {
-            query: sql,
-            params: [username]
-        }
-        crud.select(filter_data, (docs) => {
-            if(docs[0] == undefined) {
-                return done(null, false, {message: '아이디를 다시 확인해주세요' });
-            } else {
-                if (bcrypt.compareSync(password, docs[0].password)) {
-                    return done(null, [username, docs[0].password])
-                } else {
-                    return done(null, false, {message: '비밀번호를 잘못 입력하셨습니다.' });
-                }
+    passport.use(
+        new LocalStrategy(
+            {
+                usernameField: "username",
+                passwordField: "password",
+                session: true, // 세션에 저장 여부
+                passReqToCallback: true,
+            },
+            (req, username, password, done) => {
+                var sql = "SELECT * FROM tb_user WHERE user_id = ? ";
+
+                var filter_data = {
+                    query: sql,
+                    params: [username],
+                };
+                crud.select(filter_data, (docs) => {
+                    //console.log(docs[0]);
+                    if (docs[0] == undefined) {
+                        return done(null, false, { message: "아이디를 다시 확인해주세요" });
+                    } else {
+                        const encryptedPW = bcrypt.hashSync(password, 10);
+                        console.log(encryptedPW, docs[0].USER_PWD);
+                        // 비밀번호가 맞으면
+                        if (bcrypt.compare(encryptedPW, docs[0].USER_PWD)) {
+                            return done(null, [username, docs[0].USER_PWD]);
+                        } else {
+                            // pw 안맞음
+                            return done(null, false, {
+                                message: "비밀번호를 잘못 입력하셨습니다.",
+                            });
+                        }
+                    }
+                });
             }
-        })
-
-    }));
-
-}
+        )
+    );
+};
