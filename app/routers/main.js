d041822f17febe7187fe2424b30775dc739f6c6d
bae geuntae <geuntae0212@gmail.com>
2022-06-09 08:58:50 +0000

    Revert "Merge branch 'feature' into 'master'"
    This reverts merge request !1

--- app/routers/main.js
+++ app/routers/main.js
@@ -1,16 +1,15 @@
-var fs = require("fs");
-var path = require("path");
+var fs = require('fs');
+var path = require('path');
 
 module.exports = function (app) {
-    app.get("/", (req, res) => {
-        console.log(req.user);
-        res.render("index");
-        // var errorMsg = req.flash().error // 로그인실패시 들어오는 메세지 (!!!!!!!!!!!한번 사용하면 사라짐!!!!!!!!!!!!!!!)
-        // if (errorMsg) {
-        // 	res.send("<script> alert('" + errorMsg + "'); window.location.href = '/login'; </script>") // 에러 뜰때
-        // } else {
-        // 	var main_path = path.join(__dirname + '/../../public/views/main.html');
-        // 	res.render(main_path);
-        // }
-    });
+	app.get('/', (req, res) => {
+		res.render('main');
+		// var errorMsg = req.flash().error // 로그인실패시 들어오는 메세지 (!!!!!!!!!!!한번 사용하면 사라짐!!!!!!!!!!!!!!!)
+		// if (errorMsg) {
+		// 	res.send("<script> alert('" + errorMsg + "'); window.location.href = '/login'; </script>") // 에러 뜰때
+		// } else {
+		// 	var main_path = path.join(__dirname + '/../../public/views/main.html');
+		// 	res.render(main_path);
+		// }
+	});
 };
