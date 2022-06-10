b14a90ed81da439c69dde5955b5ab2da20c9d2b6
qormsxo <45680973+qormsxo@users.noreply.github.com>
2022-06-10 11:14:48 +0900

    '경로설정';

--- app/routers/page_router/page_router.js
+++ app/routers/page_router/page_router.js
@@ -2,33 +2,29 @@ var passport = require('passport');
 var path = require('path');
 
 module.exports = function (app) {
-	app.get('/sub/about', (req, res) => {
-		var main_path = path.join(__dirname + '/../../../public/views/sub/about.ejs');
-
-        res.render(main_path);
-	});
+    app.get('/sub/about', (req, res) => {
+        res.render('./sub/about');
+    });
 
     app.get('/sub/qa', (req, res) => {
-		var main_path = path.join(__dirname + '/../../../public/views/sub/qa.ejs');
-
-        res.render(main_path);
-	});
+        res.render('./sub/qa');
+    });
 
     app.get('/account/register', (req, res) => {
-		var main_path = path.join(__dirname + '/../../../public/views/account/register.ejs');
-
-        res.render(main_path);
-	});
+        res.render('./account/register');
+    });
 
     app.get('/account/register_creataccount', (req, res) => {
-		var main_path = path.join(__dirname + '/../../../public/views/account/register_creataccount.ejs');
-
-        res.render(main_path);
-	});
+        res.render('./account/register_creataccount');
+    });
 
     app.get('/account/login', (req, res) => {
-		var main_path = path.join(__dirname + '/../../../public/views/account/login.ejs');
+        res.render('./account/login');
+    });
 
-        res.render(main_path);
-	});
+    app.get('/test', (req, res) => {
+        //var main_path = path.join(__dirname, '/public/views/test.html');
+
+        res.render('test');
+    });
 };
