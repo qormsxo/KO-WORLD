94f91443524a04b3d1c82438a4004e429c218f70
jeonghoHub <tjsrsdqq4@gmail.com>
2022-06-10 13:27:19 +0900

    FEAT : QA페이지 데이터테이블 세팅

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // 사용 모듈 참조
-var express = require('express'),
-	//, routes = require('./routes')
-	//, user = require('./routes/user')
-	http = require('http'),
-	path = require('path'),
-	config = require('config'),
-	session = require('express-session'),
-	cookieParser = require('cookie-parser'),
-	bodyParser = require('body-parser'),
-	fs = require('fs');
+const express = require('express'),
+    //, routes = require('./routes')
+    //, user = require('./routes/user')
+    http = require('http'),
+    path = require('path'),
+    config = require('config'),
+    session = require('express-session'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    fs = require('fs');
 
 //서버 생성
 var app = express();
@@ -18,28 +18,28 @@ var server = http.createServer(app);
 require(path.join(__dirname, 'app/pages/router.js'))(app);
 
 //정의된 경로 중 아무것도 타지 않았을때
-app.get('*', function(req, res){
-	var main_path = path.join(__dirname, '/public/views/404.ejs');
+app.get('*', function (req, res) {
+    var main_path = path.join(__dirname, '/public/views/404.ejs');
 
-	res.render(main_path);   
+    res.render(main_path);
 });
 
 app.use((req, res, next) => {
-	if (!allowedMethods.includes(req.method)) return res.send(405, 'Method Not Allowed')
-	if (req.session.passport) {
-		req.session.passport.user.session_timeout = req.session.cookie.expires;
-	}
-	return next();
+    if (!allowedMethods.includes(req.method)) return res.send(405, 'Method Not Allowed');
+    if (req.session.passport) {
+        req.session.passport.user.session_timeout = req.session.cookie.expires;
+    }
+    return next();
 });
 
 //console.log("PATH:", __dirname)
-app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
+
 //포트 설정(config에서 가져오기)
 app.set('port', config.get('port'));
 
 //서버 실행
 server.listen(app.get('port'), function () {
-	var dir = './uploadFiles';
-	if (!fs.existsSync(dir)) fs.mkdirSync(dir); //업로드 폴더 없을시 생성
-	console.log('##Server port ' + app.get('port'));
+    var dir = './uploadFiles';
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir); //업로드 폴더 없을시 생성
+    console.log('##Server port ' + app.get('port'));
 });
